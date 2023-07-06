ba6b050eafdff89a352aecb890b8b34c3941eea1
Alpay Akçer <info@alpayakcer.com>
2023-07-06 15:26:01 +0300

    Add project files.

---
+++ JqueryProject.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JqueryProject.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
